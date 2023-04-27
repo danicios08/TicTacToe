96c57156a672b55087417663eb8f4050216f3988
danicios08 <131496378+danicios08@users.noreply.github.com>
2023-04-27 12:41:40 -0400

    Add files via upload

---
+++ src/main/java/TicTacToe/TicTacToe.java
@@ -0,0 +1,11 @@
+package TicTacToe;
+
+public class TicTacToe {
+
+    private final char J1 = '0';
+    private final char J2 = '0';
+    private final char EMPTY = '-';
+
+    public TicTacToe(){
+    }
+}
